6d58c1fd1313e9932c98fa4e7bb30e71d57496a3
Dinesh18042001 <dinesh@1804200182@gmail.com>
2024-10-17 15:23:19 +0530

    add some page

--- src/Component/Career.jsx
+++ src/Component/Career.jsx
@@ -64,7 +64,7 @@ export default function Career() {
                         </div>
 
                         <div className="carrer-card-btn text-center p-3">
-                            <Link to="/digitalmarketer">Learn More</Link>
+                            <Link to="/digitalmarketermain">Learn More</Link>
                         </div>
                         
                     </div>
--- src/Component/Courses/DigitalMarketer/DigitalMarketerMain.jsx
+++ src/Component/Courses/DigitalMarketer/DigitalMarketerMain.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import DigitalMarketerBanner from './DigitalMarketerBanner'
+import Companies from "../../Companies";
 
 export default function DigitalMarketerMain() {
   return (
     <>
     <DigitalMarketerBanner/>
+    <Companies/>
     </>
   )
 }
